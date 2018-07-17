3ddd8ac005ecac619e1562eac31ecb0a9b27c077
xa-xa-xa <28684394+xa-xa-xa@users.noreply.github.com>
2018-07-17 14:09:40 -0700

    almost final, has one bug with toogling classes of css  in main.js file -  when user resizing window

--- js/main.js
+++ js/main.js
@@ -23,23 +23,53 @@ $(() => {
 // }
 
 //menu
+var windowWidth = window.innerWidth;
 
-$('.menu-btn').click(function () {
-    $('#topics').toggleClass('nav-open');
-    $('#hamburger, main, code, footer').toggleClass('change');
+
+
+window.addEventListener("resize", function () {
+    console.log("---" + this.innerWidth);
+    if (innerWidth < 600) {
+        console.log("resized to mobile!")
+        $('#topics').click(function () {
+            $('#topics').removeClass('nav-open');
+            $('#hamburger').removeClass('change');
+        });
+    }
+    if (innerWidth > 599) {
+        console.log("resized to desktop!")
+        $('#topics').removeClass('nav-open');
+        $('#hamburger, main, code, footer').removeClass('change');
+    }
 });
+
+
+$(function widthOnLoad() {
+    $('.menu-btn').click(function () {
+        $("#hamburger").toggleClass('change');
+        $('#topics').toggleClass('nav-open');
+    });
+
+    console.log('with on load - ' + windowWidth)
+
+    if (windowWidth < 600) { //menu functionality specific for mobile   
+        console.log("it is moble!")
+        $('#topics').click(function () {
+            $('#topics').toggleClass('.nav-open');
+            $('#hamburger').toggleClass('change');
+        })
+    }
+    if (windowWidth > 599) { //menu functionality for desktop
+        console.log("it is desktop!")
+        $('.menu-btn').click(function () {
+            $('main, code, footer').toggleClass('change');
+        });
+    };
+});
+// ***********************************************************//
+
+//to show sub menu of 5.1
 $('.arrow').click(function () {
     $('#sub').toggleClass('show-list');
 
 })
-
-
-$(window).resize(function() {
-    if ($(window).width() <= 600) {
-        $('.smoothScroll').click(function () {
-            $('#topics').removeClass('nav-open');
-            $('#hamburger').removeClass('change');
-        });   
-
-    }
-});
